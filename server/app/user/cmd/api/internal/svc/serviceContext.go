ebae90aec157f80623ca88df522b22df5f58ef8a
笑脸带眼红 <202606540@qq.com>
2023-07-14 21:52:05 +0800

    1、添加 user rpc 代码 2、user api 实力话 user rpc 客户端 3、添加 log 配置

--- server/app/user/cmd/api/internal/svc/serviceContext.go
+++ server/app/user/cmd/api/internal/svc/serviceContext.go
@@ -1,20 +1,39 @@
 package svc
 
 import (
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
 	"server/app/user/cmd/api/internal/config"
+	"server/app/user/cmd/rpc/pb"
+	"server/app/user/cmd/rpc/user"
 	"server/common/middleware"
 )
 
 type ServiceContext struct {
-	Config config.Config
-	Auth   rest.Middleware
+	Config  config.Config
+	Auth    rest.Middleware
+	UserRpc user.User
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 
 	return &ServiceContext{
-		Config: c,
-		Auth:   middleware.NewAuthMiddleware().Handle, // 认证中间件
+		Config:  c,
+		Auth:    middleware.NewAuthMiddleware().Handle, // 认证中间件
+		UserRpc: NewUserRpc(c.UserRpc),
 	}
+}
+
+// NewUserRpc
+// Author [SliverFlow]
+// @desc user rpc 连接
+func NewUserRpc(c zrpc.RpcClientConf) pb.UserClient {
+	client, err := zrpc.NewClient(c)
+	if err != nil {
+		logx.Errorf("[RPC CONNECTION ERROR] user rpc client conn err: %v\n ", err)
+		return nil
+	}
+	logx.Info("[RPC CONNECTION SUCCESS	] user rpc client conn success \n")
+	return user.NewUser(client)
 }
---
+++ server/app/user/cmd/rpc/internal/logic/getuserinfobyidlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"server/app/user/cmd/rpc/internal/svc"
+	"server/app/user/cmd/rpc/pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetUserInfoByIdLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetUserInfoByIdLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserInfoByIdLogic {
+	return &GetUserInfoByIdLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *GetUserInfoByIdLogic) GetUserInfoById(in *pb.UserInfoReq) (*pb.UserInfoReply, error) {
+	// todo: add your logic here and delete this line
+
+	return &pb.UserInfoReply{}, nil
+}
